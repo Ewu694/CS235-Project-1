f04c16efd5a1a1315f952fd36eadb91e5bc4c7fc
Eric Wu <91401582+Ewu694@users.noreply.github.com>
2023-02-08 13:42:58 -0500

    Update Book.cpp

--- Book.cpp
+++ Book.cpp
@@ -39,7 +39,14 @@ std::string Book::getAuthor() const
 
 void Book::setPageCount(const int & book_page_count_var)
 {
+    if(book_page_count_var < 0)
+    {
+        std::cout<< "Page Count cannot be negative." << std::endl;
+    }
+    else
+    {
     book_page_count_ = book_page_count_var;
+    }
 }
 
 int Book::getPageCount() const
@@ -62,48 +69,4 @@ bool Book::isDigital() const
     {
         return false;
     }
-}
-
-int main()
-{
-    std::cout << "TEST 1: TEST DEFAULT CONSTRUCTOR & ACCESSORS" << std::endl;
-
-    // Test default constructor
-    Book my_book;
-
-    // Test accessor functions
-    std::cout << my_book.getName() << std::endl;
-    std::cout << my_book.getAuthor() << std::endl;
-    std::cout << my_book.getPageCount() << std::endl;
-    std::cout << my_book.isDigital() << std::endl;
-
-    std::cout << "\nTEST 2: TEST PARAMETERIZED CONSTRUCTOR & ACCESSORS" << std::endl;
-
-    // Test parameterized constructor
-    Book my_book1("Harry Potter and the Chamber of Secrets", "J.K Rowling", 253, 1);
-    Book my_book2("Moby Dick", "Herman Melville", 378);
-
-    // Test accessor functions
-    std::cout << my_book1.getName() << " By " << my_book1.getAuthor()
-              << " with page count " << my_book1.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book1.isDigital() << std::endl;
-
-    std::cout << my_book2.getName() << " By " << my_book2.getAuthor()
-              << " with page count " << my_book2.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book2.isDigital() << std::endl;
-
-    std::cout << "\nTEST 3: TEST MUTATOR FUNCTIONS" << std::endl;
-
-    // Test mutator functions
-    Book my_book3("Moby Dick", "Herman Melville", 378);
-    my_book3.setName("50 shades of gray");
-    my_book3.setAuthor("E. L. James");
-    my_book3.setPageCount(514);
-    my_book3.setPageCount(-10);
-    my_book3.setDigital();
-
-    // Test accessor functions
-    std::cout << my_book3.getName() << " By " << my_book3.getAuthor()
-              << " with page count " << my_book3.getPageCount() << std::endl;
-    std::cout << "Digital: " << my_book3.isDigital() << std::endl;
 }
